ecbe884261bd99202574f67e81ed70c905e38c8d
Vanshchauhan-1106 <Vanshchauhan1106@gmail.com>
2024-07-13 10:10:22 +0530

    Day 1

---
+++ Day 1/variablesAndDataTypes.js
@@ -0,0 +1,57 @@
+// Activity - 1 (Varible Declaration)
+
+var var1 = 25;
+console.log("Number Variable Declaration Using 'var'! var1 = ", var1);
+
+let var2 = "CHAI OR CODE";
+console.log("String Variable Declaration Using 'let'! var2 = ", var2);
+
+console.log("");
+
+// Activity - 2 (Constant Declaration)
+
+const var3 = true;
+console.log(
+  "Boolean Constant Variable Declaration Using 'const! var3 = '",
+  var3
+);
+
+console.log("");
+
+// Activity - 3 (Data Types)
+
+let num = 14;
+let str = "JavaScript Challenge";
+let bool = false;
+let obj = {
+  name: "Vansh",
+  age: 19,
+};
+let arr = ["Ridam", 20];
+
+console.log("Type of num variable is : ", typeof num);
+console.log("Type of str variable is : ", typeof str);
+console.log("Type of bool variable is : ", typeof bool);
+console.log("Type of obj variable is : ", typeof obj);
+console.log("Type of arr variable is : ", typeof arr);
+
+console.log("");
+
+// Activity - 4 (Reassigning Variables)
+
+let a = 11;
+console.log("Initial Value of 'a' : ", a);
+a = 22;
+console.log("Value of 'a' after reassinging: ", a);
+
+console.log("");
+
+// Activity - 5 (Understaing Constant)
+
+const b = 18;
+console.log("Initial value of constant varibale 'b' : ", b);
+// b = 45; (Gives an error bcz reassingment to constant variables is not possible )
+
+console.log("");
+
+// Activity
